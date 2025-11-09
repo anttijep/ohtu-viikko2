961f6834c745fc7f573565714a493dc244253717
Antti <antti.vas@gmail.com>
2025-11-09 20:02:59 +0200

    osa 8

--- osa2/project-reader/src/project.py
+++ osa2/project-reader/src/project.py
@@ -1,17 +1,21 @@
+from functools import reduce
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, dependencies, dev_dependencies, license=None, authors = []):
         self.name = name
         self.description = description
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
-
+        self.license = license
+        self.authors = authors
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return reduce(lambda a, d: a + "\n- " +d, dependencies, "") if len(dependencies) > 0 else ""
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license or ''}\n\n"
+            f"Authors:{self._stringify_dependencies(self.authors)}\n"
+            f"\nDependencies:{self._stringify_dependencies(self.dependencies)}\n"
+            f"\nDevelopment dependencies:{self._stringify_dependencies(self.dev_dependencies)}\n"
         )
--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import tomli
 
 
 class ProjectReader:
@@ -9,7 +10,14 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        obj = tomli.loads(content)
+        poetry = obj["tool"]["poetry"]
+        name = poetry.get("name", "")
+        desc = poetry.get("description", "")
+        deps = list(poetry.get("dependencies", {}).keys())
+        dev_deps = list(poetry.get("group", {}).get("dev", {}).get("dependencies", {}).keys())
+        license = poetry.get("license", None)
+        authors = poetry.get("authors", [])
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, desc, deps, dev_deps, license, authors)
